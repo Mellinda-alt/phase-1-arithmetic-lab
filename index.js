89af50ec1e27213a8b4902e41d2582d462aa8cee
Mellinda wambui <mellindawambui@gmail.com>
2024-06-29 12:15:44 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Write your code here //
 
-// Write your code here
+let num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2
+
+let random = 2
+
+let num3 = 8
+let num4 = 2
+
+let mod = num3 / num4
+
+let max = 20
